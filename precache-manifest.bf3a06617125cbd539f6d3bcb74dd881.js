880c372e6cde4cd1f94dcf4b4176010e9b8e42f5
Your Name <you@example.com>
2019-11-01 17:21:47 +0200

    Updates

---
+++ precache-manifest.bf3a06617125cbd539f6d3bcb74dd881.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe654a43935198115785",
+    "url": "/task-manager-with-calendar/static/js/runtime~main.d2845970.js"
+  },
+  {
+    "revision": "96f934a26d2844fc3051",
+    "url": "/task-manager-with-calendar/static/js/main.765b4238.chunk.js"
+  },
+  {
+    "revision": "c7039e4e58f2bdd72e50",
+    "url": "/task-manager-with-calendar/static/js/2.6e6c3edd.chunk.js"
+  },
+  {
+    "revision": "96f934a26d2844fc3051",
+    "url": "/task-manager-with-calendar/static/css/main.c809bdd2.chunk.css"
+  },
+  {
+    "revision": "543b3ae84a752668a0deed397c634f4d",
+    "url": "/task-manager-with-calendar/index.html"
+  }
+];
